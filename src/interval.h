54a68a38cf4e8e149f954811f383283edc541230
Nishant Holla <nishantholla19@gmail.com>
2025-05-07 12:07:14 +0530

    Document interval

--- src/interval.h
+++ src/interval.h
@@ -1,32 +1,54 @@
 #ifndef INTERVAL_H_
 #define INTERVAL_H_
 
+// ==============================
+// Interval class
+// ==============================
+
 class Interval {
   public:
-    double min, max;
+    double min, max; // range of the interval
 
+    /*
+     * Constructs the interval object with -infinity and +infinity as the range if no argument is passed.
+     */
     Interval() :
       min(+infinity),
       max(-infinity) {
       }
 
+    /*
+     * Constructs the interval object with the passed min and max range.
+     */
     Interval(double min, double max) :
       min(min),
       max(max) {
       }
 
+    /*
+     * Returns the size of the interval
+     */
     double size() const {
       return max - min;
     }
 
+    /*
+     * Checks if a given point is part of the current interval. x belongs to [min, max]
+     */
     bool contains(double x) const {
       return min <= x && x <= max;
     }
 
+    /*
+     * Checks if a given point is inside the current interval. ie. x belongs to (min, max)
+     */
     bool surrounds(double x) const {
       return min < x && x < max;
     }
 
+    /*
+     * Clamps the given point within the current interval.
+     */
     double clamp(double x) const {
       if (x < min) {
         return min;
@@ -39,8 +61,11 @@ class Interval {
       return x;
     }
 
-    static const Interval empty, universe;
+    static const Interval empty; // Interval that contains no points
+    static const Interval universe; // Interval that contains all the points
 };
+
+// Initializing empty and universe interval
 
 const Interval Interval::empty = Interval(+infinity, -infinity);
 const Interval Interval::universe = Interval(-infinity, +infinity);
