66827b0a4e3429b74ef9abd206ca0c7916e1c91a
Nishant Holla <nishantholla19@gmail.com>
2025-05-07 11:55:41 +0530

    Document ray

--- src/ray.h
+++ src/ray.h
@@ -3,31 +3,50 @@
 
 #include "vector3.h"
 
+// ==============================
+// Ray class
+// ==============================
+
 class Ray {
   public:
+    /*
+     * Constructs the ray with zero vector as origin and direction.
+     */
     Ray() {
     }
 
-    Ray(const Vector3& origin, const Vector3& direction) :
+    /*
+     * Constructs the ray with the given origin and direction vector.
+     */
+    Ray(const Point3& origin, const Vector3& direction) :
       orig(origin),
       dir(direction) {
       }
 
+    /*
+     * Returns a const reference to the origin point of the ray.
+     */
     const Point3& origin() const {
       return orig;
     }
 
+    /*
+     * Returns a const reference to the direction vector of the ray.
+     */
     const Vector3& direction() const {
       return dir;
     }
 
+    /*
+     * Returns the position of the ray at the given time unit.
+     */
     Point3 at(double t) const {
       return orig + t * dir;
     }
 
   private:
-    Point3 orig;
-    Vector3 dir;
+    Point3 orig; // origin point of the ray
+    Vector3 dir; // direction vector of the ray
 };
 
 #endif //!RAY_H_
