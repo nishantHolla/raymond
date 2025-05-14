aa6276c28c2eb59037c3ed582742c3feea50e49f
Nishant Holla <nishantholla19@gmail.com>
2025-05-14 20:50:02 +0530

    Document ImageBuffer

--- src/image_buffer.h
+++ src/image_buffer.h
@@ -17,18 +17,31 @@
 #pragma GCC diagnostic pop
 #endif
 
+// ==============================
+// ImageBuffer class
+// ==============================
+
 class ImageBuffer {
   public:
+    /*
+     * Constructor for the image buffer with the given width and height of the image
+     */
     ImageBuffer(int width, int height) :
       width(width),
       height(height) {
         buffer = new Color[width * height * 3];
       }
 
+    /*
+     * Get color refrence to given row and column of the pixel in the buffer
+     */
     Color& get(int row, int col) {
       return buffer[row * width + col];
     }
 
+    /*
+     * Write the current image buffer to the given file path using stbi
+     */
     void write_to_file(const std::string& file_path, const std::string& extension) {
       unsigned char* image = new unsigned char[width * height * 3];
 
@@ -52,14 +65,17 @@ class ImageBuffer {
       delete[] image;
     }
 
+    /*
+     * Free the image buffer
+     */
     ~ImageBuffer() {
       delete[] buffer;
     }
 
   private:
-    int width;
-    int height;
-    Color *buffer;
+    int width;       // width of the image
+    int height;      // height of the image
+    Color *buffer;   // buffer to hold the color values of the pixels
 };
 
 #endif //!IMAGE_BUFFER_H_
